95d02a5b057e253125d54e8eeee76400611b6ce1
ebeltz <ebbeltz@gmail.com>
2022-01-15 13:13:34 -0500

    add questions and fix markup for README

--- Develop/README.md
+++ Develop/README.md
@@ -1,10 +1,25 @@
-# coding
-  # coding
-  # coding
-  # coding
-  # coding
-  # coding
-  # coding
-  # coding
-  undefined
+# Coding
+  ![Github licence](http://img.shields.io/badge/license-undefined-blue.svg)
   
+  ## Description 
+  undefined
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  undefined
+  ## Usage 
+  undefined
+  ## License 
+  This project is license under undefined
+  ## Contributing 
+  undefined
+  ## Tests
+  undefined
+  ## Questions
+  If you have any questions about this projects, please contact me directly at undefined. You can view more of my projects at https://github.com/undefined.
--- Develop/index.js
+++ Develop/index.js
@@ -5,23 +5,96 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project title?'
-      },
-      {
-        type: 'checkbox',
-        message: 'What languages do you know?',
-        name: 'stack',
-        choices: ['HTML', 'CSS', 'JavaScript', 'MySQL']
-      },
-      {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis']
-      }
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+    } 
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your email address!');
+            return false; 
+        }
+    }
+
+},
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is your project name?',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your project name!');
+            return false; 
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project.',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a description of your project!');
+            return false; 
+        }
+    }
+}, 
+{
+    type: 'input',
+    name: 'install',
+    message: 'What are the steps required to install your project?',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter steps required to install your project!');
+            return false; 
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this app?',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a usage description!');
+            return false; 
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'test', 
+    message: 'What command should be run to run tests?',
+    default: 'npm test'
+},
+{
+    type: 'input',
+    name: 'contributors',
+    message: 'What does the user need to know about contributing to the repo?'
+}
 ]
 
 
